eb598f1f3ef4ae05cb52554516dd2c0b3c482000
Francis Surroca <francis.surroca@gmail.com>
2020-04-12 21:47:10 -0400

    Add list of templates under catalog. Has bugs

--- src/components/Catalog.js
+++ src/components/Catalog.js
@@ -7,7 +7,8 @@ class Catalog extends React.Component {
   constructor() {
     super();
     this.state = {
-      resources : "",
+      catalogs : "",
+      templatesToCatalog : "",
       token : ""
     }
   }
@@ -26,9 +27,42 @@ class Catalog extends React.Component {
       .then(response => response.json())
       .then(result => {
         console.log(result)
-        console.log(result.resources)
-        console.log(result.resources[0])
-        this.setState( {resources: result.resources})
+        this.setState( {catalogs: result.resources}, () => {
+          this.state.catalogs.forEach((catalog) => {
+            this.getCatalogTemplates(catalog.id)
+          })
+        })
+      })
+      .catch(error => console.log('error', error));
+  }
+
+  getCatalogTemplates(catalogId) {
+    console.log("getCatalogTemplates")
+    var myHeaders = new Headers();
+    myHeaders.append("X-Auth-Token", this.state.token);
+
+    var requestOptions = {
+      method: 'GET',
+      headers: myHeaders,
+      redirect: 'follow'
+    };
+
+    var url = "https://miq-db-12.lvn.broadcom.net/api/service_catalogs/" + catalogId + "/service_templates?expand=resources"
+    console.log(url)
+    fetch(url, requestOptions)
+      .then(response => response.json())
+      .then(result => {
+        console.log(result)
+        var templatesToCatalog = {}
+        this.state.catalogs.map((catalog, index) => {
+          var template = result.resources.map((template, index) => { 
+            return template.service_template_catalog_id === catalog.id ? template : null
+          })
+          console.log("catalog id: " + catalog.id + " template: " + template)
+          templatesToCatalog[catalog.id] = template
+        })        
+      
+        this.setState( {templatesToCatalog: templatesToCatalog})
       })
       .catch(error => console.log('error', error));
   }
@@ -47,12 +81,14 @@ class Catalog extends React.Component {
   }
 
   render() {    
-    const {resources} = this.state;
-    console.log(resources)
+    const {catalogs} = this.state;
+    const {templatesToCatalog} = this.state
+    console.log(catalogs)
+    console.log(templatesToCatalog)
 
-    if (!resources) {
+    if (!catalogs || !templatesToCatalog) {
       return (
-        <div className="Home">
+        <div className="Catalog">
           <div className="lander">
             <h1>Loading</h1>            
             <Clock />
@@ -61,21 +97,27 @@ class Catalog extends React.Component {
       );
     }
 
-    const renderedResourcesList = resources.map((resource, index) => {
+    const renderedResourcesList = catalogs.map((catalog, index) => {
       return (
-        <div className="card mx-auto" style={{"margin" : "8px"}} key={resource.id}>
-        <div className="card-header"><b>{resource.name}</b></div>
+        <div className="card mx-auto" style={{"margin" : "8px"}} key={catalog.id}>
+        <div className="card-header"><b>{catalog.name}</b></div>
         <div className="card-body">          
-          <h6 className="card-subtitle mb-2 text-muted">Id: {resource.id}</h6>  
-          <h6 className="card-subtitle mb-2 text-muted">Description: {resource.description}</h6>  
-          <h6 className="card-subtitle mb-2 text-muted">Service Template: {resource.service_template_id}</h6>            
-          
+          <h6 className="card-subtitle mb-2 text-muted">Id: {catalog.id}</h6>  
+          <h6 className="card-subtitle mb-2 text-muted">Description: {catalog.description}</h6>  
+          <h6 className="card-subtitle mb-2 text-muted">Tenant Id: {catalog.tenant_id}</h6>      
+          <h6 className="card-subtitle mb-2 text-muted" style={{"font-size": "70%"}}>
+            Templates: 
+              {templatesToCatalog[catalog.id].map((template, index) => 
+                { 
+                  return template != null ? <li key={template.guid}><a href={template.href}>{template.name}</a></li> : null }) 
+                }
+          </h6>  
         </div>
     </div>);
     });
 
     return (
-      <div className="Home">
+      <div className="Catalog">
         {renderedResourcesList}        
       </div>
     );
