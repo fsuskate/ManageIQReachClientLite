eca5bfa683ad092a17266d06e7648eaf36a0f968
Francis Surroca <francis.surroca@gmail.com>
2020-04-16 13:08:16 -0400

    Fxied indents

--- src/services/CatalogService.js
+++ src/services/CatalogService.js
@@ -1,21 +1,21 @@
 class CatalogService {
     static getCatalogs(token, callback) {
-    var myHeaders = new Headers();
-    myHeaders.append("X-Auth-Token", token);
-    console.log(myHeaders)
+      var myHeaders = new Headers();
+      myHeaders.append("X-Auth-Token", token);
+      console.log(myHeaders)
 
-    var requestOptions = {
-      method: 'GET',
-      headers: myHeaders,
-      redirect: 'follow'
-    };
+      var requestOptions = {
+        method: 'GET',
+        headers: myHeaders,
+        redirect: 'follow'
+      };
 
-    fetch("https://miq-db-12.lvn.broadcom.net/api/service_catalogs?expand=resources", requestOptions)
-      .then(response => response.json())
-      .then(result => {
-        callback(result)
-      })
-      .catch(error => console.log('error', error));
+      fetch("https://miq-db-12.lvn.broadcom.net/api/service_catalogs?expand=resources", requestOptions)
+        .then(response => response.json())
+        .then(result => {
+          callback(result)
+        })
+        .catch(error => console.log('error', error));
     }
     
     static getCatalogTemplates(token, catalogId, callback) {
