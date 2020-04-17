6c6de472c32374d5435d2b48949f46ef77acf80a
Francis Surroca <francis.surroca@gmail.com>
2020-04-16 21:39:20 -0400

    Use proxy in package.json for all requests not in the apps routes
    see https://create-react-app.dev/docs/proxying-api-requests-in-development/

--- src/services/CatalogService.js
+++ src/services/CatalogService.js
@@ -39,6 +39,43 @@ class CatalogService {
         })
         .catch(error => console.log('error', error));
       }
+
+    static postProvisionTemplate(token, callback) {
+      var myHeaders = new Headers()
+      myHeaders.append("x-auth-token", token)
+      myHeaders.append("Content-Type", "text/plain");
+
+      var payload = {
+        "action": "order",
+        "resource": {
+          "href": "https://miq-db-12.lvn.broadcom.net/api/service_catalogs/12000000000002/service_templates/12000000000011",
+          "ems_dropdown": "12000000000015",
+          "template": "12000000000538",
+          "cpu_size": 1,
+          "memory_size": 4,
+          "disk_size": 60,
+          "service_name": "Francis-Test-Centos-Networking-24",
+          "vm_name": "renbdl",
+          "instance_type": 12000000000401    
+        }
+      }
+
+      var requestOptions = {
+        method: 'POST',
+        headers: myHeaders,
+        body: JSON.stringify(payload),
+        redirect: 'follow'
+      };
+
+      fetch("/api/service_catalogs/12000000000002/service_templates/12000000000011", requestOptions)
+        .then(response => response.text())
+        .then(result => {
+          callback(result)
+        })
+        .catch(error => {
+          console.log('error', error)
+        });
+    }
 }
 
 export default CatalogService
